65968ecb2336cbef96f48c0c66c51f7fb9dae60a
almina-mac <alm.0554.mina@gmail.com>
2020-05-31 11:06:00 +0900

    Update: add View classes (for movie, audio, sensor, label)

---
+++ src/viewMovie.cpp
@@ -0,0 +1,8 @@
+//
+//  viewMovie.cpp
+//  sensor_data_visualizer
+//
+//  Created by Almina on 2020/05/30.
+//
+
+#include "viewMovie.hpp"
---
+++ src/viewMovie.hpp
@@ -0,0 +1,13 @@
+//
+//  viewMovie.hpp
+//  sensor_data_visualizer
+//
+//  Created by Almina on 2020/05/30.
+//
+
+#ifndef viewMovie_hpp
+#define viewMovie_hpp
+
+#include <stdio.h>
+
+#endif /* viewMovie_hpp */
